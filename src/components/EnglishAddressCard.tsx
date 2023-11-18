c1bce8c5535c438cdec284685797d91258ffea70
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-11-18 09:57:24 +0700

    Refactor EnglishAddressCard component

--- src/components/EnglishAddressCard.tsx
+++ src/components/EnglishAddressCard.tsx
@@ -1,27 +1,16 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, SyntheticEvent } from 'react'
 import { Autocomplete, CardHeader, CardContent, Grid, Typography, TextField } from '@mui/material'
 
 type AddressType = 'ProvinceEn' | 'AmphoeEn' | 'TambonEn' | 'ZipcodeEn'
 
 interface EnglishAddressCardProps {
-  thaiAddressEN: any[]
-  provinceEn: any
-  amphoeEn: any
-  tambonEn: any
-  zipcodeEn: any
+  data: any[]
+  state: any
   onChange: (type: AddressType, event: any, newValue: any) => void
-  onSelect: (type: AddressType, event: any, newValue: any) => void
+  onSelect: (type: AddressType, event: SyntheticEvent<Element, Event>, newValue: any) => void
 }
 
-const EnglishAddressCard: React.FC<EnglishAddressCardProps> = ({
-  thaiAddressEN,
-  provinceEn,
-  amphoeEn,
-  tambonEn,
-  zipcodeEn,
-  onChange,
-  onSelect,
-}) => {
+const EnglishAddressCard: React.FC<EnglishAddressCardProps> = ({ data, state, onChange, onSelect }) => {
   return (
     <Fragment>
       <CardHeader
@@ -55,13 +44,13 @@ const EnglishAddressCard: React.FC<EnglishAddressCardProps> = ({
                   id='combo-box-province-en'
                   size='small'
                   fullWidth
-                  options={thaiAddressEN.map((province: any, index: number) => {
+                  options={data.map((province: any, index: number) => {
                     return { key: index, label: province[0] }
                   })}
                   isOptionEqualToValue={(option, value) => option.key === value.key && option.label === value.label}
                   onChange={(event: any, value: any) => onChange('ProvinceEn', event, value)}
                   renderInput={(params) => <TextField {...params} label='จังหวัด' />}
-                  onInputChange={(event: any, value: any) => onSelect('ProvinceEn', event, value)}
+                  onInputChange={(event: SyntheticEvent<Element, Event>, value: any) => onSelect('ProvinceEn', event, value)}
                 />
               </Grid>
 
@@ -71,11 +60,11 @@ const EnglishAddressCard: React.FC<EnglishAddressCardProps> = ({
                   id='combo-box-amphoe-en'
                   size='small'
                   fullWidth
-                  disabled={provinceEn?.key === 0 && provinceEn?.label === '' ? true : false}
-                  value={amphoeEn}
+                  disabled={state.provinceEn?.key === 0 && state.provinceEn?.label === '' ? true : false}
+                  value={state.amphoeEn}
                   options={
-                    amphoeEn && thaiAddressEN[provinceEn?.key] && thaiAddressEN[provinceEn.key][1][amphoeEn.key]
-                      ? (thaiAddressEN[provinceEn.key][1] as any[]).map((amphoe: any, index: number) => {
+                    state.amphoeEn && data[state.provinceEn?.key] && data[state.provinceEn.key][1][state.amphoeEn.key]
+                      ? (data[state.provinceEn.key][1] as any[]).map((amphoe: any, index: number) => {
                           return { key: index, label: amphoe[0] }
                         })
                       : []
@@ -83,7 +72,7 @@ const EnglishAddressCard: React.FC<EnglishAddressCardProps> = ({
                   isOptionEqualToValue={(option, value) => option.key === value.key && option.label === value.label}
                   onChange={(event: any, value: any) => onChange('AmphoeEn', event, value)}
                   renderInput={(params) => <TextField {...params} label='อำเภอ' />}
-                  onInputChange={(event: any, value: any) => onSelect('AmphoeEn', event, value)}
+                  onInputChange={(event: SyntheticEvent<Element, Event>, value: any) => onSelect('AmphoeEn', event, value)}
                 />
               </Grid>
 
@@ -93,11 +82,11 @@ const EnglishAddressCard: React.FC<EnglishAddressCardProps> = ({
                   id='combo-box-tambon-en'
                   size='small'
                   fullWidth
-                  disabled={amphoeEn?.key === 0 && amphoeEn?.label === '' ? true : false}
-                  value={tambonEn}
+                  disabled={state.amphoeEn?.key === 0 && state.amphoeEn?.label === '' ? true : false}
+                  value={state.tambonEn}
                   options={
-                    amphoeEn && thaiAddressEN[provinceEn?.key] && thaiAddressEN[provinceEn.key][1][amphoeEn.key]
-                      ? (thaiAddressEN[provinceEn.key][1][amphoeEn.key][1] as any[]).map((tambon: any, index: number) => {
+                    state.amphoeEn && data[state.provinceEn?.key] && data[state.provinceEn.key][1][state.amphoeEn.key]
+                      ? (data[state.provinceEn.key][1][state.amphoeEn.key][1] as any[]).map((tambon: any, index: number) => {
                           return { key: index, label: tambon[0] }
                         })
                       : []
@@ -105,7 +94,7 @@ const EnglishAddressCard: React.FC<EnglishAddressCardProps> = ({
                   isOptionEqualToValue={(option, value) => option.key === value.key && option.label === value.label}
                   onChange={(event: any, value: any) => onChange('TambonEn', event, value)}
                   renderInput={(params) => <TextField {...params} label='ตำบล' />}
-                  onInputChange={(event: any, value: any) => onSelect('TambonEn', event, value)}
+                  onInputChange={(event: SyntheticEvent<Element, Event>, value: any) => onSelect('TambonEn', event, value)}
                 />
               </Grid>
 
@@ -115,11 +104,14 @@ const EnglishAddressCard: React.FC<EnglishAddressCardProps> = ({
                   id='combo-box-zipcode-en'
                   size='small'
                   fullWidth
-                  disabled={tambonEn?.key === 0 && tambonEn?.label === '' ? true : false}
-                  value={zipcodeEn}
+                  disabled={state.tambonEn?.key === 0 && state.tambonEn?.label === '' ? true : false}
+                  value={state.zipcodeEn}
                   options={
-                    tambonEn && thaiAddressEN[provinceEn?.key] && thaiAddressEN[provinceEn.key][1][amphoeEn?.key]
-                      ? (thaiAddressEN[provinceEn.key][1][amphoeEn.key][1][tambonEn.key][1] as any[]).map(
+                    state.tambonEn &&
+                    data[state.provinceEn?.key] &&
+                    data[state.provinceEn.key][1][state.amphoeEn?.key] &&
+                    data[state.provinceEn.key][1][state.amphoeEn.key][1][state.tambonEn.key]
+                      ? (data[state.provinceEn.key][1][state.amphoeEn.key][1][state.tambonEn.key][1] as any[]).map(
                           (zipcode: any, index: number) => ({
                             key: index,
                             label: zipcode.toString(),
@@ -130,7 +122,7 @@ const EnglishAddressCard: React.FC<EnglishAddressCardProps> = ({
                   isOptionEqualToValue={(option, value) => option.key === value.key && option.label === value.label}
                   onChange={(event: any, value: any) => onChange('ZipcodeEn', event, value)}
                   renderInput={(params) => <TextField {...params} label='รหัสไปรษณีย์' />}
-                  onInputChange={(event: any, value: any) => onSelect('ZipcodeEn', event, value)}
+                  onInputChange={(event: SyntheticEvent<Element, Event>, value: any) => onSelect('ZipcodeEn', event, value)}
                 />
               </Grid>
             </Grid>
